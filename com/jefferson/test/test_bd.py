1d72c9dbabf6c3403724bd525b63005e37cd2952
Jefferson <jefferson.paixao@aluno.faculdadeimpacta.com.br>
2020-04-18 04:51:48 -0300

    atualizando com comentários

--- com/jefferson/test/test_bd.py
+++ com/jefferson/test/test_bd.py
@@ -4,3 +4,6 @@ from com.jefferson.bd import bancodedados
 def test_criando_tabela():
     banco = bancodedados()
     assert  banco.criando_tabela("'id'","pk",1) == True, "Deveria ser True"
+    #1º parametro = qual coluna da tabela criada você quer testar ("id", "nome" ou "email")
+    #2º parametro = qual atributo da coluna você quer testar ("pk" ou "notnull")
+    #3º parametro = qual valor esperado no teste (1 ou 0)
