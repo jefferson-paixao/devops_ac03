ff5ad388e86e0c99dac799eb1ce36a985e3e7731
Jefferson <jefferson.paixao@aluno.faculdadeimpacta.com.br>
2020-04-18 02:36:02 -0300

    ajustando o nome da tabela sem aspas

--- com/jefferson/bd.py
+++ com/jefferson/bd.py
@@ -9,7 +9,7 @@ class bancodedados():
             content = content_file.read()
             c.execute(content)
 
-        c.execute('SELECT * FROM PRAGMA table_info("ac3") WHERE name = '+coluna)
+        c.execute('SELECT * FROM pragma_table_info(ac3) WHERE name = '+coluna)
         for row in c.fetchall():
            notnull = row[3]
            pk = row[5]
